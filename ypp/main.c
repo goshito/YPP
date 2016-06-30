85821c3e41a6485e764a836fc77d2e6d50acf734
georgi <georgi@georgi-Aspire-E5-572G>
2016-06-30 23:35:41 +0300

    It has begun..

---
+++ ypp/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: georgi
+ *
+ * Created on June 30, 2016, 11:34 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Pesho Peshev - peshistia");
+
+    return (EXIT_SUCCESS);
+}
+

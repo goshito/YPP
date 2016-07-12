646b636ca6558df9eb9e0d71b45a217e03677469
georgi <georgi@georgi-Aspire-E5-572G>
2016-07-12 18:48:56 +0300

    progress so far, main menu added, not finished nor compiled

--- ypp/main.c
+++ ypp/main.c
@@ -17,13 +17,16 @@
  * TO DO 12.07.2016:
  * What exactly does readln function and how does it do it? -replaced with gets()
  * When user enters that he/she uses more than one medicine keep asking until 4 medicines are entered
- * temp_med should go into an array
+ * temp_med should go into an array - I already have an array(where to put it?
  *  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
+//#define filename "medicine_database.bin"
+#define backupfilename "medicine_database.bak"
 #define MAXSTRLEN 200
 
 struct medicine {
@@ -43,7 +46,7 @@ void init_temp_med() {
     strcpy(temp_med.producer, "");
 }
 
-void read_meds_data() {
+void read_meds_data() { // what should happend after that? what to do with the temp_med structure?
     char medicine_name[MAXSTRLEN];
     int mg;
     char dosage[MAXSTRLEN];
@@ -53,8 +56,7 @@ void read_meds_data() {
     init_temp_med(); //give initial values because if nothing is here the garbage values will
     
     while (slen == 0) {
-        printf("\nEnter medicine name: ");
-        //slen = readln(medicine_name); // what is slen for?
+        printf("\nEnter medicine name: ");        
         getchar(); // flush
         slen = gets(medicine_name); // what does gets() return - This function returns str on success, and NULL on error or when end of file occurs, while no characters have been read.
     }
@@ -62,8 +64,6 @@ void read_meds_data() {
     slen = 0;
     while (slen == 0) {
         printf("\nEnter mg: ");
-        //slen = readln(mg); // convert it to number using atoi() later?
-        //slen = (int)gets(mg);
         scanf("%i", &mg);
         slen = mg;
         getchar(); // flush
@@ -90,11 +90,52 @@ void read_meds_data() {
     
 }
 
+void add_medicine(char *filename) {
+    FILE *f;
+    
+    read_meds_data();
+    f = fopen(filename, "rb");
+    if (f == 0) {
+        printf("Cannot write to file: %s\n", filename);
+    } else {
+        fwrite(&temp_med, sizeof(temp_med), 1, f);
+        fclose(f);
+    }
+}
+
 int main(int argc, char** argv) {
-    int meds_count; 
+    int meds_count, keep_going = 1;
+    char choice;
     
     printf("WELCOME TO YPP(Your Pills Program)\n\n");
-    printf("How many medicines you use? ");
+    printf("Please select an option:\n");
+    
+    while (keep_going) {
+        switch(choice) {
+        case 'a': // add a medicine in database
+            printf("Add a medicine to the database\n");
+            add_medicine("medicine_database.bin");
+            break;
+        case 'd':
+            printf("Display medicines\n");
+            // fill what's necessary here
+            break;
+        case 'm':
+            printf("Modify medicine\n");
+            // fill what's necessary here
+            break;
+        case 'a':
+            printf("Ending...\n");
+            keep_going = 0;
+            break;
+        default:
+            printf("Invalid command.\n");
+            break;
+        }
+    }
+    
+    
+    printf("How many medicines you use? "); //this must be beneath case 'a'
     scanf("%i", &meds_count); // create an array of struct for each medicine
     struct medicine meds_database[meds_count]; //create a struct array to store meds
     
